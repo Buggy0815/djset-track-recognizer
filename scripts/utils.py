9086ffafd3f3e5a84247541aba979b266330cd3f
Sebastian Weiler <sebastianweiler@hotmail.com>
2023-07-07 16:48:46 +0200

    modified utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,70 @@
+import pandas as pd
 from ShazamAPI import Shazam
 import youtube_dl
 import os
+from pydub import AudioSegment
+import io
+from multiprocessing import Pool
 
+def get_tracklist_of_url(url, output_path):
+    '''
+    main function
+    '''
+    title = url.split('/')[-2] + '_' + url.split('/')[-1]
+    whole_audio_to_file(url, output_path) #download and save audio
+    audio = AudioSegment.from_file(f'{output_path}complete/{title}.mp3', format='mp3') #load audio
+    chunks = chunking(audio)
+    tracks = get_tracks(chunks)
+    df = tracks_to_df(tracks)
+    return df
+    
+def tracks_to_df(tracks):
+    df = pd.DataFrame(tracks)
+    df = df.dropna()
+    df = df.drop_duplicates()
+    df['artist'] = df[0].apply(lambda x: x[0]).copy()
+    df['track'] = df[0].apply(lambda x: x[1]).copy()
+    df = df.drop(columns=[0])
+    df = df.drop_duplicates().rename(columns = {0:'artist', 1:'track'})
+    return df
+
+def get_tracks(chunks):
+    tracks = []
+    with Pool() as pool:
+        # call the same function with different data in parallel
+        for result in pool.map(get_artist_title, chunks):
+            # report the value to show progress
+            tracks.append(result)
+    return tracks
+    
+def chunking(audio):
+    chunks = []
+    chunk_length_ms = 30*1000   # 30-second chunks
+    delay = 4 *60*1000            # every 2 minutes
+    count = 40*1000             # starting at 40 seconds
+
+    while True:
+        start_time = count
+        end_time = count + chunk_length_ms
+
+        # Extract the desired chunk from the audio
+        audio_chunk = audio[start_time:end_time]
+
+        # Export the audio chunk as an MP3 file to a BytesIO object
+        audio_file = io.BytesIO()
+        audio_chunk.export(audio_file, format='mp3')
+
+        # Read the audio data from the BytesIO object
+        audio_data = audio_file.getvalue()
+
+        # Add the audio data to the list
+        chunks.append(audio_data)
+
+        count = count + chunk_length_ms + delay
+        if count + chunk_length_ms > len(audio):
+            break          
+    return chunks
+    
 def whole_audio_to_file(url, output_path):
     title = url.split('/')[-2] + '_' + url.split('/')[-1]
     options = {
@@ -10,6 +73,7 @@ def whole_audio_to_file(url, output_path):
     }
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([url])
+    print('downloaded')
 
 def get_artist_title(file):
     '''
